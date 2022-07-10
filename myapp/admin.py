5dda21a9a1fc6279e09276bc3a746579089094cb
django-dev-uz <player2020uz@gmail.com>
2022-07-10 10:59:35 +0500

    first commit

---
+++ myapp/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+
+from .models import GetInfo
+from .models import PhotoFor
+from .models import AddProject
+from .models import AddInformation
+from .models import AddInformation1
+from .models import ProjectCategory
+
+admin.site.register(GetInfo)
+admin.site.register(PhotoFor)
+admin.site.register(AddProject)
+admin.site.register(ProjectCategory)
+admin.site.register(AddInformation)
+admin.site.register(AddInformation1)
